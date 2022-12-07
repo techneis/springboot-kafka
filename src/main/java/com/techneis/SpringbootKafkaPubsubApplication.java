cf233cd7fbfa2c7a22ac7706cd4c1133969650ee
Neeraj <techneis@gmail.com>
2022-12-07 18:46:49 +0530

    Initial Commit

---
+++ src/main/java/com/techneis/SpringbootKafkaPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.techneis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaPubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techneis/SpringbootKafkaPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techneis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaPubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
